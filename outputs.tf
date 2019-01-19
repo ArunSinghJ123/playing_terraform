12ae540886ca254c3433cb583a9ee6f48060f035
Arunsingh <arun@Arunsinghs-MBP.home>
2019-01-18 22:24:36 -0800

    peering

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-ioutput "vpc_id" {
+output "vpc_id" {
   description = "The ID of the VPC"
   value       = "${element(concat(aws_vpc.this.*.id, list("")), 0)}"
 }
--- vpc/account-prod/main.tf
+++ vpc/account-prod/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
+module "vpc" "east"{
   source = "../../"
 
-  name = "prod-example"
+  name = "prod-example-1"
 
   cidr = "10.10.0.0/16"
 
@@ -26,10 +26,57 @@ module "vpc" {
   enable_dhcp_options              = true
   dhcp_options_domain_name         = "arunsingh.co"
   dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
+  tags = {
+    Owner       = "user"
+    Environment = "staging"
+    Name        = "Unroutable-VPC"
+  }
+}
+
+module "urvpc" "east"{
+  source = "../../"
+
+  name = "prod-example-2"
+
+  cidr = "172.16.0.0/16"
+
+  azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets     = ["172.16.0.0/24", "172.16.1.0/24", "172.16.2.0/24"]
+  public_subnets      = ["172.16.3.0/24", "172.16.4.0/24", "172.16.5.0/24"]
+
+  create_database_subnet_group = false
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_vpn_gateway = false
+
+  enable_s3_endpoint       = true
+  enable_dynamodb_endpoint = true
+
+  enable_dhcp_options              = true
+  dhcp_options_domain_name         = "arunsingh.co"
+  dhcp_options_domain_name_servers = ["127.0.0.1", "172.16.0.2"]
 
   tags = {
     Owner       = "user"
     Environment = "staging"
-    Name        = "complete"
+    Name        = "Unroutable-VPC"
+  }
+}
+
+data "aws_caller_identity" "current" { }
+
+resource "aws_vpc_peering_connection" "foo" {
+  peer_owner_id = "${data.aws_caller_identity.current.account_id}"
+  peer_vpc_id   = "${module.urvpc.vpc_id}"
+  vpc_id        = "${module.vpc.vpc_id}"
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
   }
 }
