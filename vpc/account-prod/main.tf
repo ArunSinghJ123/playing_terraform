df5ebea7ae22c384c235fed7ed757ac1c1a758e5
Arunsingh <arun@Arunsinghs-MBP.home>
2019-01-18 23:23:17 -0800

    security group approach

--- vpc/account-prod/main.tf
+++ vpc/account-prod/main.tf
@@ -27,7 +27,7 @@ module "vpc" "east"{
   dhcp_options_domain_name         = "arunsingh.co"
   dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
   tags = {
-    Owner       = "user"
+    Owner       = "Arun"
     Environment = "staging"
     Name        = "Unroutable-VPC"
   }
@@ -78,5 +78,26 @@ resource "aws_vpc_peering_connection" "foo" {
 
   requester {
     allow_remote_vpc_dns_resolution = true
+  }
+}
+
+locals {
+  name_prefix         = ["${module.vpc.vpc_id}", "${module.urvpc.vpc_id}"]
+}
+
+resource "aws_security_group" "http" {
+  count       = 2
+  name        = "HTTPfromOut"
+  description = "Allow all HTTP traffic"
+  vpc_id      = "${element(local.name_prefix, count.index)}"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  tags = {
+    Name = "HTTPfromOut"
   }
 }
